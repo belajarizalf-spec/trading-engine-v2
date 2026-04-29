28a27d59d2775454d259866769a44b85dd3d23a7
belajarizalf-spec <belajarizalf@gmail.com>
2026-04-29 15:19:06 +0700

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -32,7 +32,7 @@ module.exports = async function handler(req, res) {
 
   if (!TOKEN) return res.status(500).json({ code: -1, msg: 'TOKEN MISSING' });
 
-  const BASE = 'https://api.itick.id';
+  const BASE = 'https://api.itick.org';
 
   try {
     let url = '';
